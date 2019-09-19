8f3b7c2b4564fc2c5ef15be8ae3efdfa25d1587c
FranciF12 <francesco.perrotta1@studenti.uniparthenope.it>
2019-09-19 18:40:39 +0200

    Update dbCoonnection
    Added payment table, ticket table,

--- CinePerry/db/dbConnection.py
+++ CinePerry/db/dbConnection.py
@@ -354,9 +354,9 @@ class ConnectionClass(object):
                 conn.close()
                 print("MySQL connection is closed")
    
-###################################################################
-#                         PERSON TABLE                            #
-###################################################################
+        ###################################################################
+        #                         PERSON TABLE                            #
+        ###################################################################
 
     """INSERIMENTO VALORE PERSONA IN PERSONA
        Inserimento del valore di Persona nel database Registrazione iniziale nel Database
@@ -483,5 +483,165 @@ class ConnectionClass(object):
             print("MySQL connection is closed")
 
         return myresult
+     ###################################################################
+    #                         TICKET TABLE                            #
+    ###################################################################
+
+    """ INSERIMENTO DEL TICKET NELLA TABELLA DEI TICKET
+        la funzione permette di registrare nel dabatabase tutti i biglietti che verranno acquistati """  # OK
+
+    def insertvalueticket(self, titleFilm, nameSala, nameP, surnameP, date, time, price, nTickets, tipoSpettacolo,
+                          numberGlasses, codPerson):
+
+        # now to create a query for inserting value
+        query = "INSERT INTO ticket(titleFilm, nameSala, nameP, surnameP, date, time, price, nTickets, tipoSpettacolo,\
+                numberGlasses, codPerson) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+        args = (
+            titleFilm, nameSala, nameP, surnameP, date, time, price, nTickets, tipoSpettacolo, numberGlasses,
+            codPerson,)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+            cursor = conn.cursor()
+            result = cursor.execute(query, args)
+            conn.commit()
+            print("Record inserted successfully into Ticket table")
+
+        except mysql.connector.Error as error:
+            conn.rollback()
+            print(f"Failed to insert into MySQL table {error}")
+
+        finally:
+            # closing database connection
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+    """ Funzione che permette di selezionare tutti i film per la tabella film"""
+
+    def select_ticketall(self):
+
+        query = "SELECT * FROM ticket "
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query)
+
+            myresult = cursor.fetchall()
+            print("Ticket")
+            for x in myresult:
+                print(x)
+
+        except Error as error:
+            print(error)
+
+        finally:
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+        return myresult
+
+    """Updating for seats """  # OK
+
+    def update_count_seats(self, seats, nameSala):
+
+        query = " UPDATE sala SET seats = %s WHERE nameSala = %s"
+        args = (seats, nameSala,)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            conn.commit()  # accept the change
+            print("Record updated successfully into Sala table")
+
+        except Error as error:
+            print(error)
+
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+    """Funzione che permette di contare tutti i ticket venduti in un 
+     determinato range per l'eventuale report"""  # OK
+
+    def select_ticket_for_report(self, name_sala, date_from, date_until):
+
+        query = "SELECT SUM(price), SUM(nTickets) FROM ticket WHERE nameSala = %s AND  date > %s AND date < %s"
+        args = (name_sala, date_from, date_until,)
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            myresult = cursor.fetchall()
+            print("EXTRACT ELEMENTS:")
+            for x in myresult:
+                print(x)
+
+        except Error as error:
+            print(error)
+
+        finally:
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+        return myresult
+
+    ###################################################################
+    #                         PAYMENT TABLE                           #
+    ###################################################################
+    """Function for inserting payment in a table Payment """  # OK
+
+    def insert_value_payment(self, idcard, typep, owner, expiriondate, cvc, price, codp):
+
+        # now to create a query for inserting value
+        query = "INSERT INTO payment(idCard, typePayment, owner, expirionDate, CVC, price, codPerson)" \
+                " VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        args = (idcard, typep, expiriondate, owner, cvc, price, codp,)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+            conn.commit()
+            print("Record inserted successfully into Person table")
+
+        except mysql.connector.Error as error:
+            conn.rollback()
+            print(f"Failed to insert into MySQL table {error}")
+
+        finally:
+            # closing database connection
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
 
 
