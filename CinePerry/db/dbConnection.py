c386c089c6d721b2bc1d77ce60de0fef2c787bfa
FranciF12 <francesco.perrotta1@studenti.uniparthenope.it>
2019-09-19 18:32:05 +0200

    Update dbConnection
    Added type theater table and theater table

--- CinePerry/db/dbConnection.py
+++ CinePerry/db/dbConnection.py
@@ -135,3 +135,222 @@ class ConnectionClass(object):
                 print("MySQL connection is closed")
 
         return myresult
+    
+    ###################################################################
+    #                       TYPE THEATER TABLE                        #
+    ###################################################################
+
+    """Function for inserting type of cinema in Type Theater """  # OK
+
+    def insert_typesala(self, tipoSpettacolo):
+
+        query = "INSERT INTO tiposala (tipoSpettacolo) VALUES (%s)"
+        args = (tipoSpettacolo,)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+            conn.commit()
+            print("Record inserted successfully into Type Theater table")
+
+        except mysql.connector.Error as error:
+            conn.rollback()
+            print(f"Failed to insert into MySQL table {error}")
+
+        finally:
+            if conn.is_connected():  # closing database connection.
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+    """ Function for selecting all type of theater """  # OK
+    """ selezione tipo spettacolo nella tabella film"""
+
+    def selectalltypespett(self):
+        query = "SELECT * FROM tiposala"
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query)
+
+            myresult = cursor.fetchall()
+
+            for x in myresult:
+                print(x)
+
+        except Error as error:
+            print(error)
+
+        finally:
+            if conn.is_connected():  # closing database connection.
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+        return myresult
+
+    ###################################################################
+    #                         THEATER TABLE                           #
+    ###################################################################
+
+    """ Function for creating a new sale into Sala table """  # OK
+
+    def insertsala(self, nameSala, tipoSpettacolo, dateSala, timeSala, price, seats, titleFilmE):
+
+        # now to create a query for inserting value
+        query = "INSERT INTO sala(nameSala,tipoSpettacolo, dateSala, timeSala, price, seats, titleFilmE)\
+                 VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        args = (nameSala, tipoSpettacolo, dateSala, timeSala, price, seats, titleFilmE)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+            conn.commit()
+            print("Record inserted successfully into Sala table")
+
+        except mysql.connector.Error as error:
+            conn.rollback()
+            print(f"Failed to insert into MySQL table {error}")
+
+        finally:
+            if conn.is_connected():  # closing database connection
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+    """ Function for selecting all name of sala """  # OK
+    """elenca il nome di tutte le sale """
+
+    def selectallnamesala(self):
+
+        query = "SELECT * FROM sala"
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query)
+
+            myresult = cursor.fetchall()
+
+            for x in myresult:
+                print(x)
+
+
+        except Error as error:
+            print(error)
+
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+        return myresult
+
+    # seats from sala                                            #OK
+    def select_seats(self, name_sala):
+
+        query = "SELECT seats FROM sala WHERE nameSala = %s "
+        args = (name_sala,)
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            myresult = cursor.fetchall()
+
+            for x in myresult:
+                print(x)
+
+        except Error as error:
+            print(error)
+
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+        return myresult
+
+    """ Update function for updating the elements of theater"""  # OK
+
+    def updatevaluesala(self, tipoS, dateS, timeS, price, seats, titleFilmE, nameSala):
+
+        query = " UPDATE sala SET tipoSpettacolo = %s, dateSala = %s, timeSala = %s, price = %s, seats = %s,\
+                  titleFilmE = %s WHERE nameSala = %s"
+
+        args = (tipoS, dateS, timeS, price, seats, titleFilmE, nameSala,)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            conn.commit()  # accept the change
+            print("Record updated successfully into Sala table")
+
+        except Error as error:
+            print(error)
+
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
+    """ Funzione che mi permette di impostare a null la chiave esterna di sala, per poi cancellare """  # OK
+    """ il film con la funzione deleteValueFilm """
+
+    def setnulltitle(self, title):
+
+        query = " UPDATE sala SET titleFilmE = null WHERE titleFilmE = %s "
+        args = (title,)
+
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='BiglietteriaCinema',
+                                           user='root',
+                                           password='')
+
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            conn.commit()  # accept the change
+            print("Record updated successfully into Sala table")
+
+        except Error as error:
+            print(error)
+
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+                print("MySQL connection is closed")
+
