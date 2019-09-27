81480a35e728eb16b1601c176ccf4320e16f22e8
FranciF12 <francesco.perrotta1@studenti.uniparthenope.it>
2019-09-27 20:49:17 +0200

    edit-js

--- CinePerry/static/js/js.js
+++ CinePerry/static/js/js.js
@@ -1,1 +1,11 @@
 /** FUNCTION JS **/
+/** Function for slide bar */
+function openNav() { //open slide bar
+    document.getElementById("mySidebar").style.width = "60%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeNav() { //close slide bar
+    document.getElementById("mySidebar").style.display = "none";
+}
+
